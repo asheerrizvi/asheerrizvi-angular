2e203e31cbe4fbff18a082922e6d2ef8b4b29955
unknown <asheer.vil@gmail.com>
2019-07-20 00:52:31 +0530

    Pushed the skeletal code for Repos. Some changes and minor improvements to follow.

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { REPOSITORIES } from '../../data/repository-data';
+
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
+  public repositories = REPOSITORIES;
 
   constructor() { }
 
---
+++ src/app/models/repository.ts
@@ -0,0 +1,7 @@
+export class Repository {
+    img: string;
+    heading: string;
+    tech: string[];
+    description: string;
+    link: string;
+}
